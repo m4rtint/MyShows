268edb542070c047c715561219be6b6eea24481e
Martin <m4rtin.t@gmail.com>
2017-01-05 01:23:50 +0800

    fixed migration file

--- database/migrations/2017_01_04_172250_add_uncensored_to_shows.php
+++ database/migrations/2017_01_04_172250_add_uncensored_to_shows.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateShowsTable extends Migration
+class AddUncensoredToShows extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreateShowsTable extends Migration
      */
     public function up()
     {
-        Schema::create('shows', function (Blueprint $table) {
+        Schema::table('shows', function (Blueprint $table) {
             $table->string('id');
             $table->string('title')->unique();
             $table->longText('description');
+            $table->boolean('uncensored');
             $table->string('image_link');
             $table->string('video_link');
         });
@@ -29,6 +30,6 @@ class CreateShowsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('shows');
+        //
     }
 }
